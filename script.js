08a785b4688c490b94f770f5591d6c0fa08a7e23
Oliver Mänder <oliver.maender@gmail.com>
2021-03-11 00:51:09 +0100

    After video 17 with several own modifications

--- script.js
+++ script.js
@@ -1,23 +1,31 @@
 let canvas;
 let ctx;
-let character_x = 0;
-let character_y = 300;
+let character_x = 100;
+let character_y = 150;
 let isMovingRight = false;
 let isMovingLeft = false;
 let lastJumpStarted = 0;
 let bg_elements = 0;
+let currentCharacterImage = './img/pepe/idle/I-1.png';
+let characterGraphicsRight = ['./img/pepe/idle/I-1.png', './img/pepe/walking/W-22.png', './img/pepe/walking/W-25.png'];
+let characterGraphicsLeft = ['./img/pepe/idle/I-1.png', './img/pepe/walking/W-22.png', './img/pepe/walking/W-25.png'];
+let characterGraphicsIndex = 0;
+let cloudOffset = 0;
+let backgroundPosition = 0;
 
 // Game config
 
 let JUMP_TIME = 300; // in ms
+let GAME_SPEED = 4;
 
 function init() {
 
     canvas = document.getElementById('canvas');
     ctx = canvas.getContext("2d");
+    checkForRunning();
 
     draw();
-
+    calculateCloudOffset();
     listenForKeys();
 
 }
@@ -25,14 +33,14 @@ function init() {
 function updateCharacter() {
 
     let base_image = new Image();
-    base_image.src = './img/character_1.png';
+    base_image.src = currentCharacterImage;
 
     let timePassedSinceJump = new Date().getTime() - lastJumpStarted;
     if (timePassedSinceJump < JUMP_TIME) {
         character_y = character_y - 4;
     } else {
 
-        if (character_y < 300) {
+        if (character_y < 150) {
             character_y = character_y + 4;
         }
     }
@@ -47,9 +55,44 @@ function updateCharacter() {
     // }
 
     if (base_image.complete) {
-        ctx.drawImage(base_image, character_x, character_y, base_image.width * 0.5, base_image.height * 0.5);
+        ctx.drawImage(base_image, character_x, character_y, base_image.width * 0.3, base_image.height * 0.3);
     };
 
+}
+
+function checkForRunning() {
+
+    setInterval(function() {
+
+        if(isMovingRight) {
+
+            let index = characterGraphicsIndex % characterGraphicsRight.length;
+
+            currentCharacterImage = characterGraphicsRight[index];
+            characterGraphicsIndex++;
+    
+        }
+    
+        if(isMovingLeft) {
+
+            let index = characterGraphicsIndex % characterGraphicsRight.length;
+
+            currentCharacterImage = characterGraphicsLeft[index];
+            characterGraphicsIndex++;
+            
+        }
+
+    }, 50);
+    
+}
+
+function calculateCloudOffset() {
+    
+    setInterval(function() {
+
+        cloudOffset = cloudOffset + 0.5;
+
+    }, 50);
 }
 
 function draw() {
@@ -66,34 +109,81 @@ function drawBackground() {
     ctx.fillStyle = "white";
     ctx.fillRect(0, 0, canvas.width, canvas.height);
 
+    drawBackgroundObject('./img/background/clouds/1.png', 0 - cloudOffset, 0, 0.5, 0.5, 1);
+
 }
 
 function drawGround() {
 
-    ctx.fillStyle = "#ddbc00";
-    ctx.fillRect(0, canvas.height, canvas.width, -100);
+    // ctx.fillStyle = "#ddbc00";
+    // ctx.fillRect(0, canvas.height, canvas.width, -100);
+
+        // drawBackgroundObject('./img/background/background3/1.png', (0 - backgroundPosition), 0, 0.54, 0.54, 1);
+        // drawBackgroundObject('./img/background/background2/1.png', (0 - backgroundPosition), 0, 0.54, 0.54, 1);
+        // drawBackgroundObject('./img/background/background1/1.png', (0 - backgroundPosition), 0, 0.54, 0.54, 1);
+
+        for (let i = 0; i < 10; i = i + 2) {
+
+            drawBackgroundObject('./img/background/background1/complete.png', (0 - backgroundPosition) + canvas.width * i, 0, 0.534, 0.534, 1);
+            
+        }
+
+        // drawBackgroundObject('./img/background/background1/complete.png', (0 - backgroundPosition), 0, 0.534, 0.534, 1);
+        // drawBackgroundObject('./img/background/background1/complete.png', (0 - backgroundPosition) + canvas.width * 4, 0, 0.534, 0.534, 1);
+        // drawBackgroundObject('./img/background/background1/complete.png', (0 - backgroundPosition) + canvas.width * 6, 0, 0.534, 0.534, 1);
+
+    // if(!isMovingLeft && !isMovingRight) {
+    //     drawBackgroundObject('./img/background/background1/1.png', 0 - backgroundPosition, -400, 1, 1, 1);
+    // }
 
     if (isMovingLeft) {
-        bg_elements = bg_elements + 2;
+        // bg_elements = bg_elements + GAME_SPEED;
+        // drawBackgroundObject('./img/background/background1/1.png', 0 - backgroundPosition, -400, 1, 1, 1);
+        backgroundPosition--;
     }
 
     if (isMovingRight) {
-        bg_elements = bg_elements - 2;
+        // bg_elements = bg_elements - GAME_SPEED;
+        backgroundPosition++;
     }
 
-    let base_image1 = new Image();
-    base_image1.src = './img/bg_elem_1.png';
-    if (base_image1.complete) {
-        ctx.drawImage(base_image1, bg_elements, 235, base_image1.width * 0.8, base_image1.height * 0.8);
+}
+
+function drawBackgroundObject(src, offsetX, offsetY, scaleX, scaleY, opacity) {
+
+    if(opacity != undefined) {
+        ctx.globalAlpha = opacity;
+    }
+
+    let base_image = new Image();
+    base_image.src = src;
+    if (base_image.complete) {
+        ctx.drawImage(base_image, bg_elements + offsetX, offsetY, base_image.width * scaleX, base_image.height * scaleY);
     };
 
-    let base_image2 = new Image();
-    base_image2.src = './img/bg_elem_2.png';
-    if (base_image2.complete) {
-        ctx.drawImage(base_image2, bg_elements + 600, 135, base_image2.width * 0.8, base_image2.height * 0.8);
-    };
+    ctx.globalAlpha = 1;
 
 }
+
+// function drawBackgroundObjectReverse(src, offsetX, offsetY, scaleX, scaleY, opacity) {
+
+//     if(opacity != undefined) {
+//         ctx.globalAlpha = opacity;
+//     }
+
+//     let base_image = new Image();
+//     base_image.src = src;
+//     ctx.save();
+//     ctx.translate(canvas.width, 0);
+//     ctx.scale(-1, 1);
+//     if (base_image.complete) {
+//         ctx.drawImage(base_image, bg_elements + offsetX, offsetY, base_image.width * scaleX, base_image.height * scaleY);
+//     };
+//     ctx.restore();
+
+//     ctx.globalAlpha = 1;
+
+// }
 
 function listenForKeys() {
 
@@ -105,12 +195,12 @@ function listenForKeys() {
 
         if (k == 'ArrowRight') {
             isMovingRight = true;
-            character_x = character_x + 10;
+            // character_x = character_x + 10;
         }
 
         if (k == 'ArrowLeft') {
             isMovingLeft = true;
-            character_x = character_x - 10;
+            // character_x = character_x - 10;
         }
 
         let timePassedSinceJump = new Date().getTime() - lastJumpStarted;
